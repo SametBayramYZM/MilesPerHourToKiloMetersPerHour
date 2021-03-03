919a5410fd9f205534d70fb1a3fb857424f070f8
SametBayramYZM <sametbayramofficial@gmail.com>
2021-03-04 00:06:53 +0300

    MPH To KMPH

---
+++ MphToKmph/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace MphToKmph
+{
+    partial class MphToKMph
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbKMph = new System.Windows.Forms.Label();
+            this.tbMph = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(157, 194);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Miles Per Hours :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kilometers Per Hours :";
+            // 
+            // lbKMph
+            // 
+            this.lbKMph.AutoSize = true;
+            this.lbKMph.Location = new System.Drawing.Point(129, 150);
+            this.lbKMph.Name = "lbKMph";
+            this.lbKMph.Size = new System.Drawing.Size(0, 13);
+            this.lbKMph.TabIndex = 3;
+            // 
+            // tbMph
+            // 
+            this.tbMph.Location = new System.Drawing.Point(132, 97);
+            this.tbMph.Name = "tbMph";
+            this.tbMph.Size = new System.Drawing.Size(100, 20);
+            this.tbMph.TabIndex = 4;
+            // 
+            // MphToKMph
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 286);
+            this.Controls.Add(this.tbMph);
+            this.Controls.Add(this.lbKMph);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "MphToKMph";
+            this.Text = "MphToKMph";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbKMph;
+        private System.Windows.Forms.TextBox tbMph;
+    }
+}
+
---
+++ MphToKmph/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MphToKmph
+{
+    public partial class MphToKMph : Form
+    {
+        public MphToKMph()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            double MPH = Convert.ToDouble(tbMph.Text);
+            double KMPH = MPH * 0.44704 * 3600 / 1000;
+            lbKMph.Text = Convert.ToString(KMPH);
+        }
+    }
+}
